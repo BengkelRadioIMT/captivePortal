d58d0510fa85de1bb8605821a7516596c22dfe75
mamanberliansyah <jungman.nurdin@gmail.com>
2018-07-31 16:57:27 +0700

    added lots of updates

---
+++ old_start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd /home/pi/captivePortal
+
+#Remove old firewall
+sudo iptables -F
+sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+sudo iptables -t mangle -F
+sudo iptables -t mangle -X
+
+#Config new firewall
+IPTABLES=/sbin/iptables
+CPTADDR=192.168.5.1
+$IPTABLES -N internet -t mangle
+$IPTABLES -t mangle -A PREROUTING -j internet
+$IPTABLES -t mangle -A internet -j MARK --set-mark 99
+$IPTABLES -t nat -A PREROUTING -m mark --mark 99 -p tcp --dport 80 -j DNAT --to-destination $CPTADDR
+echo "1" > /proc/sys/net/ipv4/ip_forward
+$IPTABLES -A FORWARD -i eth0 -o wlan0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+$IPTABLES -A FORWARD -m mark --mark 99 -j REJECT
+$IPTABLES -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+$IPTABLES -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+#Start Captive Portal Server
+python reset.py
+sudo python server.py
--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 from flask import Flask, request, redirect, render_template, flash
-import urllib, os, time
+import urllib, os, time, json
+
+rule_count = 0
 
 DEBUG = True
 app = Flask(__name__)
@@ -9,30 +11,83 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 def login_user(ip):
-    os.system("sudo iptables -I internet 1 -t mangle -s " + ip + " -j RETURN")
+    with open('conn.json') as json_data_file:
+        conn = json.load(json_data_file)
+        json_data_file.close()
+    k = str(conn['connCount'] + 1)
+    conn[k]['isConnect'] = 1
+    conn[k]['IP'] = ip
+    conn[k]['timeStart'] = time.time()
+    conn['connCount'] = conn['connCount'] + 1
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST' and 'login' in request.form and 'password' in request.form:
-        if (request.form['login']=='admin') and (request.form['password']=='admin'):
-            login_user(request.remote_addr)
-            flash('Login Successful')
-            time.sleep(1)
-            return redirect('https://www.google.co.id')
-        else:
-             flash('Account Invalid')
-             return redirect("/")
-    else:
-        return render_template('login.html', orig_url=urllib.urlencode({'orig_url': request.args.get('orig_url', '')}))
+    os.system("sudo iptables -I internet " + k + " -t mangle -s " + ip + " -j RETURN")
+
+    connect = open('conn.json', "w")
+    connect.write(json.dumps(conn, indent=4, sort_keys=True))
+    connect.close()
+
+def logout_user(ip):
+    with open('conn.json') as json_data_file:
+        conn = json.load(json_data_file)
+        json_data_file.close()
+    isFound = 0
+    i = 0
+    while (isFound == 0) and (i < 4) :
+        i = i + 1
+        j = str(i)
+        if (request.remote_addr == conn[j]['IP']):
+            conn[j]['isConnect'] = 0
+            conn[j]['IP'] = ''
+            conn[j]['timeStart'] = ''
+            os.system("sudo iptables -D internet " + j + " -t mangle")
+            conn['connCount'] = conn['connCount'] - 1
+            isFound = 1
+
+    connect = open('conn.json', "w")
+    connect.write(json.dumps(conn, indent=4, sort_keys=True))
+    connect.close()
+
+@app.route('/logout')
+def logout():
+    ip = request.remote_addr
+    logout_user(ip)
+    return redirect("/")
 
 @app.route('/favicon.png')
 def favicon():
     return app.send_static_file('favicon.png')
 
 @app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return redirect("http://192.168.5.1/login?" + urllib.urlencode({'orig_url': request.url}))
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def index(path):
+    with open('conn.json') as json_data_file:
+        conn = json.load(json_data_file)
+        json_data_file.close()
+    isFound = 0
+    i = 0
+    while (isFound == 0) and (i < conn['connCount']) :
+        i = i + 1
+        j = str(i)
+        if (request.remote_addr == conn[j]['IP']):
+            isFound = 1
+    if ((isFound) and  conn[j]['isConnect'] and (conn[j]['IP'] == request.remote_addr)):
+        ip = request.remote_addr
+        temp = (time.time() - conn[j]['timeStart'])
+        hours = temp//3600
+        temp = temp - 3600*hours
+        minutes = temp//60
+        seconds = temp - 60*minutes
+        timeS = '%dh %dm %ds' %(hours,minutes,seconds)
+        return render_template('success.html',ip=ip,timeS=timeS)
+    else:
+        if request.method == 'POST' and 'login' in request.form and 'password' in request.form:
+            if (request.form['login']=='admin') and (request.form['password']=='admin'):
+                login_user(request.remote_addr)
+                flash('Login Successful')
+            else:
+                flash('Account Invalid')
+            return redirect('/')
+        return render_template('login.html')
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=80)
